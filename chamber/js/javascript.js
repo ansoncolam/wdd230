605f9c9a0be7b588037901a9eb7133f183d13dd8
ansoncolam <lam22015@byui.edu>
2022-10-06 20:45:30 +0800

    updated

---
+++ chamber/js/javascript.js
@@ -0,0 +1,17 @@
+let text = document.lastModified;
+document.getElementById("lastmodified").innerHTML = text;
+
+const datefield = document.getElementById("header_date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full"}).format(now);
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.getElementById("hamburgerBtn");
+x.onclick = toggleMenu;
+
+
