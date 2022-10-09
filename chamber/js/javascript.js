91798109e989a9190fdc916cd10bbf73649af4c2
ansoncolam <lam22015@byui.edu>
2022-10-09 11:27:15 +0800

    updated

--- chamber/js/javascript.js
+++ chamber/js/javascript.js
@@ -6,6 +6,10 @@ const now = new Date();
 const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full"}).format(now);
 datefield.innerHTML = `<em>${fulldate}</em>`;
 
+let curryear = now.getFullYear();
+document.getElementById("curryear").innerHTML =`@${curryear} Tolo Chamber`;
+document.getElementById("curryear2").innerHTML =`@${curryear} Tolo Chamber | Anson Lam | WDD 230 Project |`;
+
 function toggleMenu() {
     document.getElementById("primaryNav").classList.toggle("open");
     document.getElementById("hamburgerBtn").classList.toggle("open");
