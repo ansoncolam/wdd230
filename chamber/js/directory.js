7eedad246d95c117251ee821705a4600f41d284a
ansoncolam <lam22015@byui.edu>
2022-11-12 16:09:48 +0800

    updated

--- chamber/js/directory.js
+++ chamber/js/directory.js
@@ -1,5 +1,5 @@
-const requestURL = "./json/data.js";
-const cards = document.querySelector('.cards');
+const requestURL = "./json/data.json";
+const cards = document.querySelector(".cards");
 
 fetch(requestURL)
   .then(function (response) {
@@ -7,25 +7,38 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     console.table(jsonObject);
-    const companies = jsonObject['companies'];
+    const companies = jsonObject["companies"];
     companies.forEach(displayCompanies);
   });
 
 
 function displayCompanies(company) {
-    let card = document.createElement('section');
-    let h2 = document.createElement('h2');
-    let logo = document.createElement('img');
+    let card = document.createElement("section");
+    let h2 = document.createElement("h2");
+    let logo = document.createElement("img");
+    let address = document.createElement("div");
+    let phone = document.createElement("div");
+    let url = document.createElement("a");
+    let membership = document.createElement("div");
 
     h2.textContent = `${company.name}`;
+    address.textContent = `Address: ${company.address}`;
+    phone.textContent = `Phone: ${company.phone}`;
+    url.textContent = `Website: ${company.url}`;
+    url.setAttribute("href", `${company.url}`);
+    membership.textContent = `Membership Level: ${company.membership}`;
 
-    logo.setAttribute('src', company.img);
-    logo.setAttribute('alt', `${company.name} logo`);
-    logo.setAttribute('loading', 'lazy');
+    logo.setAttribute("src", company.img);
+    logo.setAttribute("alt", `${company.name} logo`);
+    logo.setAttribute("loading", "lazy");
   
     card.appendChild(h2);
     card.appendChild(logo);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(url);
+    card.appendChild(membership);
 
-    document.querySelector('div.cards').appendChild(card);
+    document.querySelector("div.cards").appendChild(card);
   }
 
