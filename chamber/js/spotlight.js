95f052043ee39b9d18f727629d8a0e139d42baf1
ansoncolam <lam22015@byui.edu>
2022-11-25 00:16:54 +0800

    updated

--- chamber/js/spotlight.js
+++ chamber/js/spotlight.js
@@ -1,3 +1,4 @@
+
 const requestURL = "./json/data.json";
 const spotlight = document.querySelector(".sect5");
 
@@ -7,45 +8,63 @@ fetch(requestURL)
   })
   .then(function (jsonObject) {
     const companies = jsonObject["companies"];
-    companies.forEach(displaySpotlight);
+    for (let j = 0; j < 30; j++) {
+      displaySpotlight(companies[randomIntFromInterval(0, companies.length - 1)]);
+    }
   });
 
   var i = 0;
 
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  let complist = [];
+
 function displaySpotlight(company) {
 
+  if (complist.includes(company.name)) {
 
-    if (`${company.membership}` == "Gold" && i < 3) {
+  } else {
 
-        i = i + 1;
+    if ((`${company.membership}` == "Gold" || `${company.membership}` == "Silver") && i < 3) {
 
-        let card = document.createElement("div");
-        let h2 = document.createElement("h2");
-        let logo = document.createElement("img");
-        let phone = document.createElement("p");
-        let url = document.createElement("a");
+      complist.push(company.name);
+
+      i = i + 1;
+
+      let card = document.createElement("div");
+      let h2 = document.createElement("h2");
+      let logo = document.createElement("img");
+      let phone = document.createElement("p");
+      let url = document.createElement("a");
+  
+      h2.textContent = `${company.name}`;
+      phone.textContent = `${company.phone}`;
+      url.textContent = `${company.url}`;
+      url.setAttribute("href", `${company.url}`);
+  
+      logo.setAttribute("src", company.img);
+      logo.setAttribute("alt", `${company.name} logo`);
     
-        h2.textContent = `${company.name}`;
-        phone.textContent = `${company.phone}`;
-        url.textContent = `${company.url}`;
-        url.setAttribute("href", `${company.url}`);
-    
-        logo.setAttribute("src", company.img);
-        logo.setAttribute("alt", `${company.name} logo`);
-      
-        card.appendChild(h2);
-        card.appendChild(logo);
-        card.appendChild(document.createElement("hr"));
-        card.appendChild(url);
-        card.appendChild(phone);
-    
-        document.querySelector(".sect5").appendChild(card);
+      card.appendChild(h2);
+      card.appendChild(logo);
+      card.appendChild(document.createElement("hr"));
+      card.appendChild(url);
+      card.appendChild(phone);
+  
+      document.querySelector(".sect5").appendChild(card);
 
-        if (i == 3) {
-            card.setAttribute("class", "div1");
-        }
+      if (i == 3) {
+          card.setAttribute("class", "div1");
+      }
 
-    }
+  }
+
+  }
+
+
+
 
 
   }
